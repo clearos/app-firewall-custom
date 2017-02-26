ad99700a13fea1f53d88bd545cdde091ad6026af
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:52:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'Custom Xavfsizlik devori ilova Murakkab xavfsizlik devori qoidalar modernizatsiya qilish uchun past darajadagi vositasi beradi.';
+$lang['firewall_custom_app_name'] = 'Custom xavfsizlik devori';
+$lang['firewall_custom_command_is_not_permitted'] = 'Buyrug&#39;i yo&#39;l qo&#39;yilmaydi.';
+$lang['firewall_custom_description_invalid'] = 'Tavsif haqiqiy emas.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'Xavfsizlik devori qoida bekor.';
+$lang['firewall_custom_help_dragging'] = 'Sizning maxsus xavfsizlik devori qoidalar tartibi muhim ahamiyatga ega - tugmasini bosing va tartibini o&#39;zgartirish uchun jadvalda yuqoriga yoki pastga bir qoida harakatlantiring.';
+$lang['firewall_custom_help_iptables_constant'] = '$ IPTABLES fayl qulflash masalalarni oldini olish uchun bayroqlarni o&#39;z ichiga oladi iptables amri uchun doimiy emas. iloji bu sobit foydalaning!';
+$lang['firewall_custom_restart_required'] = 'Sizning qoidalar tartibi o&#39;zgargan bo&#39;lishi mumkin - xavfsizlik devori qayta ishga tushirish kerak.';
+$lang['firewall_custom_summary'] = 'Imkonini beradi maxsus xavfsizlik devori qoidalari ilova qilinadi. ilg&#39;or yoki yuqori tayyorlangan tarmoq muhitda mavjud xavfsizlik devori ilovalarni yordamida qo&#39;shimcha mumkin emas Murakkab xavfsizlik devori qoidalarini qo&#39;shimcha (DMZ, MultiWAN, Port Ekspeditorlik boshqalar Incoming) maxsus xavfsizlik devori yordamida amalga oshirilishi mumkin, kerakli xavfsizlik va paketli yo&#39;l-yo&#39;riq erishish uchun ilova. Ushbu modul foydalanish faqat ilg&#39;or foydalanuvchilar uchun himoyalangan bo&#39;lishi kerak.';
