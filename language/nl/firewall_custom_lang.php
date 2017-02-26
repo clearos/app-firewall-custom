ef06f228c502b82371c5d97b5b96c50c865c634f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:48:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'De Custom Firewall app biedt een low-level tool om geavanceerde firewall-regels configureren.';
+$lang['firewall_custom_app_name'] = 'Custom Firewall';
+$lang['firewall_custom_command_is_not_permitted'] = 'Command is niet toegestaan.';
+$lang['firewall_custom_description_invalid'] = 'Beschrijving is ongeldig.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'Firewall regel is ongeldig.';
+$lang['firewall_custom_help_dragging'] = 'De volgorde van uw aangepaste firewall-regels is belangrijk - klik en sleep de regel naar boven of beneden in de tabel om de volgorde te wijzigen.';
+$lang['firewall_custom_help_iptables_constant'] = '$ IPTABLES is een constante voor de iptables commando dat de vlaggetjes voor een bestand vergrendeling problemen te voorkomen omvat. Gebruik deze constante als dat mogelijk is!';
+$lang['firewall_custom_restart_required'] = 'De volgorde van de regels kunnen zijn veranderd - een firewall herstart is vereist.';
+$lang['firewall_custom_summary'] = 'Hiermee aangepaste firewall-regels worden toegevoegd. In gevorderde of sterk aangepaste netwerkomgevingen, de toevoeging van geavanceerde firewall-regels die niet kan worden toegevoegd met behulp van de beschikbare firewall apps (Incoming, DMZ, multi-WAN, Port Forwarding etc.) om de gewenste veiligheid en packet routing te bereiken kan worden bereikt met behulp van de aangepaste firewall app. Het gebruik van deze module moet worden gereserveerd voor alleen gevorderde gebruikers.';
