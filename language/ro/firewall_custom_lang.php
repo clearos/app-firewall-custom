41f14965d23de9545ce4921085ac50ed28a675d2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:49:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'Aplicația personalizat Firewall oferă un instrument de nivel scăzut pentru a configura reguli avansate de firewall.';
+$lang['firewall_custom_app_name'] = 'Firewall personalizat';
+$lang['firewall_custom_command_is_not_permitted'] = 'Comandă nu este permisă.';
+$lang['firewall_custom_description_invalid'] = 'Descrierea este nevalid.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'regula firewall este nevalid.';
+$lang['firewall_custom_help_dragging'] = 'Ordinea în care regulile de firewall personalizat este important - faceți clic și trageți o regulă în sus sau în jos în tabel pentru a schimba ordinea.';
+$lang['firewall_custom_help_iptables_constant'] = '$ Iptables este o constantă pentru comanda iptables, care include steaguri pentru a preveni problemele de blocare fișiere. Vă rugăm să folosiți această constantă atunci când este posibil!';
+$lang['firewall_custom_restart_required'] = 'Ordinea în care regulile pot fi modificate - este necesară o repornire firewall.';
+$lang['firewall_custom_summary'] = 'Permite reguli firewall personalizate care urmează să fie adăugate. În medii avansate sau foarte personalizate de rețele, adăugarea unor reguli avansate de firewall care nu pot fi adăugate folosind disponibile aplicații de firewall (de intrare, DMZ, MultiWAN, Port Forwarding, etc) pentru a obține de securitate și de pachete de rutare dorită poate fi realizată cu ajutorul firewall personalizat app. Folosirea acestui modul ar trebui să fie rezervat doar pentru utilizatorii avansați.';
