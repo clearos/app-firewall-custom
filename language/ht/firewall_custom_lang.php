32368e09e8495a86097dcf4291b3d114e5944ad8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:44:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'App a Custom Firewall bay yon zouti ki ba-nivo nan konfigirasyon règ firewall avanse.';
+$lang['firewall_custom_app_name'] = 'Custom Firewall';
+$lang['firewall_custom_command_is_not_permitted'] = 'Kòmandman pa pèmèt.';
+$lang['firewall_custom_description_invalid'] = 'Deskripsyon se valab.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'Firewall règ se valab.';
+$lang['firewall_custom_help_dragging'] = 'Lòd la nan règ koutim firewall ou enpòtan - Sur Et faites yon règ monte oswa desann nan tablo a chanje lòd la.';
+$lang['firewall_custom_help_iptables_constant'] = '$ Iptables se yon konstan pou yo lòd, ki iptables ki gen ladan drapo yo anpeche pwoblèm dosye bloke. Tanpri itilize konstan sa a lè posib!';
+$lang['firewall_custom_restart_required'] = 'ka Lòd la nan règ ou te chanje - se yon rekòmanse firewall yo mande yo.';
+$lang['firewall_custom_summary'] = 'Pèmèt règ firewall Customized yo dwe te ajoute. Nan yon anviwonman rezo avanse oswa trè Customized, adisyon a nan règ firewall avanse ki pa ka ajoute lè l sèvi avèk apps yo pare-feu disponib (fèk ap rantre, DMZ, MultiWAN, Port èkspédision elatriye) reyalize vle sekirite ak pake routage nan kapab akonpli lè l sèvi avèk pare-feu lan koutim app. Sèvi ak nan modil sa a ta dwe rezève pou itilizatè avanse sèlman.';
