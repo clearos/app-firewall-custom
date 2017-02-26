f0f23af10559920c0912bf212d14a1d10b1cb39a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:46:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'საბაჟო Firewall ოთახი უზრუნველყოფს დაბალი დონის ინსტრუმენტი კონფიგურაციის მოწინავე firewall წესები.';
+$lang['firewall_custom_app_name'] = 'Custom Firewall';
+$lang['firewall_custom_command_is_not_permitted'] = 'Command არ არის ნებადართული.';
+$lang['firewall_custom_description_invalid'] = 'აღწერა არასწორია.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'Firewall წესი არასწორია.';
+$lang['firewall_custom_help_dragging'] = 'რათა თქვენი საბაჟო firewall წესები მნიშვნელოვანია - დაწკაპეთ და გადაათრიეთ წესი ან ქვემოთ მაგიდაზე შეცვლის მიზნით.';
+$lang['firewall_custom_help_iptables_constant'] = '$ IPTABLES არის მუდმივი, რომ iptables ბრძანება, რომელიც მოიცავს დროშები თავიდან ასაცილებლად ფაილის საკეტი საკითხები. გთხოვთ, გამოიყენოთ ეს მუდმივი, როდესაც შესაძლებელია!';
+$lang['firewall_custom_restart_required'] = 'რათა თქვენი წესები შეიძლება შეიცვალოს - ზიხართ გადატვირთვა საჭირო.';
+$lang['firewall_custom_summary'] = 'საშუალებას მორგებული firewall წესები დაემატოს. მოწინავე და მაღალი მორგებულია ქსელის გარემოში, დამატებით მოწინავე firewall წესები, რომ არ შეიძლება დაემატოს გამოყენებით შესაძლებელი firewall apps (შემომავალი, DMZ, MultiWAN, პორტის გადამისამართება და ა.შ.), რათა მივაღწიოთ სასურველ უსაფრთხოებისა და პაკეტი routing შეიძლება განხორციელდეს გამოყენებით საბაჟო firewall აპლიკაცია. გამოყენება ამ მოდულის უნდა დაცულია წინასწარი მომხმარებლებს მხოლოდ.';
