87773a7a7f67af011786a09fb9fb343b8e5b4440
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:52:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'Các ứng dụng Tuỳ chỉnh Firewall cung cấp một công cụ cấp thấp để cấu hình các rule tường lửa tiên tiến.';
+$lang['firewall_custom_app_name'] = 'Firewall tùy chỉnh';
+$lang['firewall_custom_command_is_not_permitted'] = 'Lệnh là không được phép.';
+$lang['firewall_custom_description_invalid'] = 'Mô tả không hợp lệ.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'quy tắc tường lửa là không hợp lệ.';
+$lang['firewall_custom_help_dragging'] = 'Thứ tự các quy tắc tường lửa tùy chỉnh của bạn là quan trọng - nhấp và kéo một quy tắc lên hoặc xuống trong bảng để thay đổi thứ tự.';
+$lang['firewall_custom_help_iptables_constant'] = '$ Iptables là một hằng số cho các lệnh iptables bao gồm lá cờ để ngăn chặn các vấn đề tập tin khóa. Vui lòng sử dụng hằng số này khi có thể!';
+$lang['firewall_custom_restart_required'] = 'Thứ tự các quy tắc của bạn có thể đã thay đổi - một khởi động lại tường lửa là cần thiết.';
+$lang['firewall_custom_summary'] = 'Cho phép tùy chỉnh quy tắc tường lửa để thêm vào. Trong môi trường mạng tiên tiến hoặc tùy biến cao, việc bổ sung các quy tắc tường lửa tiên tiến mà không thể được thêm vào bằng cách sử dụng các ứng dụng tường lửa có sẵn (Incoming, DMZ, MultiWAN, Port Forwarding vv) để đạt được sự an toàn và gói mong muốn định tuyến có thể được thực hiện bằng cách sử dụng tường lửa tùy chỉnh ứng dụng. Sử dụng mô-đun này nên dành cho những người dùng cao cấp.';
