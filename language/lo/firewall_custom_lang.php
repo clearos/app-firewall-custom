bcb71c0354b8933aa68ad399590c53f56a9975af
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:46:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'ການ app Custom Firewall ເປັນເຄື່ອງມືທີ່ລະດັບຕ່ໍາການກໍາຫນົດຄ່າກົດລະບຽບໂຄງການກ້າວຫນ້າທາງດ້ານ.';
+$lang['firewall_custom_app_name'] = 'Firewall Custom';
+$lang['firewall_custom_command_is_not_permitted'] = 'ຄໍາສັ່ງບໍ່ໄດ້ອະນຸຍາດໃຫ້.';
+$lang['firewall_custom_description_invalid'] = 'ລາຍລະອຽດບໍ່ຖືກຕ້ອງ.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'ກົດລະບຽບ Firewall ແມ່ນບໍ່ຖືກຕ້ອງ.';
+$lang['firewall_custom_help_dragging'] = 'ຄໍາສັ່ງຂອງກົດລະບຽບໂຄງການລູກຄ້າຂອງທ່ານເປັນສິ່ງສໍາຄັນ - ກົດແລະ drag ກົດຂຶ້ນຫຼືຫຼຸດລົງໃນຕາຕະລາງການປ່ຽນແປງຄໍາສັ່ງຂອງ.';
+$lang['firewall_custom_help_iptables_constant'] = '$ iptables ເປັນຄົງທີ່ສໍາລັບຄໍາສັ່ງ iptables ທີ່ປະກອບມີສະຖານະການທີ່ຈະປ້ອງກັນບໍ່ໃຫ້ບັນຫາໄຟລັອກ. ກະລຸນາໃຊ້ຄົງທີ່ໃນເວລາທີ່ເປັນໄປໄດ້!';
+$lang['firewall_custom_restart_required'] = 'ຄໍາສັ່ງຂອງການລະບຽບການຂອງທ່ານອາດຈະມີການປ່ຽນແປງ - ເປັນ restart ໂຄງການແມ່ນຕ້ອງການ.';
+$lang['firewall_custom_summary'] = 'ອະນຸຍາດໃຫ້ກົດລະບຽບໂຄງການລູກຄ້າທີ່ຈະໄດ້ຮັບການເພີ່ມ. ໃນສະພາບແວດລ້ອມເຄືອຂ່າຍແບບພິເສດຫຼືທີ່ກໍາຫນົດເອງສູງ, ນອກຈາກນັ້ນກົດລະບຽບ Firewall ເປັນພິເສດທີ່ບໍ່ສາມາດໄດ້ຮັບການເພີ່ມການນໍາໃຊ້ກິດ firewall ທີ່ມີຢູ່ (Incoming, DMZ, Multiwan, Port Forwarding ແລະອື່ນໆ) ເພື່ອບັນລຸຄວາມປອດໄພແລະ packet ເສັ້ນທາງທີ່ຕ້ອງການສາມາດເຮັດໄດ້ການນໍາໃຊ້ໂຄງການທີ່ກໍາຫນົດເອງ app. ການນໍາໃຊ້ລະຫັດນີ້ຄວນຈະໄດ້ຮັບການສະຫງວນສໍາລັບຜູ້ໃຊ້ແບບພິເສດເທົ່ານັ້ນ.';
