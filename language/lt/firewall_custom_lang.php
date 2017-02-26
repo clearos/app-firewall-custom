6e5e31fd54dfcc6c7eda1302b751d8c54502cb2d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:47:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'Custom ugniasienė programa suteikia niskopoziomowym konfigūruoti užkardos taisykles.';
+$lang['firewall_custom_app_name'] = 'Pasirinktinis užkarda';
+$lang['firewall_custom_command_is_not_permitted'] = 'Komanda neleidžiama.';
+$lang['firewall_custom_description_invalid'] = 'Aprašymas negalioja.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'Ugniasienė taisyklė negalioja.';
+$lang['firewall_custom_help_dragging'] = 'Jūsų individualizuotos ugniasienės taisyklių tvarka yra svarbi - spustelėkite ir vilkite taisyklę aukštyn arba žemyn lentelėje pakeisti tvarką.';
+$lang['firewall_custom_help_iptables_constant'] = '$ Iptables yra už iptables komanda, kuri apima vėliavas siekiant užkirsti kelią failų blokavimo klausimus pastovus. Prašome naudoti šią konstantą, kai įmanoma!';
+$lang['firewall_custom_restart_required'] = 'Tavo taisykles kad galėjo pasikeisti - užkarda restartas yra būtinas.';
+$lang['firewall_custom_summary'] = 'Leidžia individualų ugniasienės taisyklės turi būti pridėta. Išsivysčiusiose arba labai specializuotų tinklų aplinkose, pažangių ugniasienės taisyklių papildymas, kuris negali būti pridėta naudojant turimus užkardos programas (Atvykstamasis, DMZ, MultiWAN, ekspedijavimas tt) pasiekti norimą saugumo ir paketų maršruto gali būti atlikta naudojant pasirinktinį užkardą progr. Naudojant šį modulį turėtų būti skirtas tik pažengusiems vartotojams.';
