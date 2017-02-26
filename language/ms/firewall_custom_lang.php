af00ee11efad8b6876a5cc1a1bdb95c6be3ef552
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:48:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'Aplikasi Custom Firewall menyediakan alat peringkat rendah untuk mengkonfigurasi peraturan firewall canggih.';
+$lang['firewall_custom_app_name'] = 'Firewall Custom';
+$lang['firewall_custom_command_is_not_permitted'] = 'Command adalah tidak dibenarkan.';
+$lang['firewall_custom_description_invalid'] = 'Penerangan tidak sah.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'peraturan Firewall adalah tidak sah.';
+$lang['firewall_custom_help_dragging'] = 'Susunan peraturan firewall peribadi anda adalah penting - klik dan seret peraturan atas atau ke bawah dalam jadual untuk mengubah perintah itu.';
+$lang['firewall_custom_help_iptables_constant'] = '$ Iptables adalah pemalar untuk arahan iptables yang termasuk bendera untuk mengelakkan isu-isu fail mengunci. Sila gunakan malar ini apabila mungkin!';
+$lang['firewall_custom_restart_required'] = 'Susunan peraturan anda mungkin telah berubah - mula semula firewall diperlukan.';
+$lang['firewall_custom_summary'] = 'Membolehkan peraturan firewall yang disesuaikan untuk ditambah. Dalam persekitaran rangkaian maju atau sangat disesuaikan, penambahan peraturan firewall canggih yang tidak boleh ditambah dengan menggunakan aplikasi firewall tersedia (Masuk, DMZ, MultiWAN, Port Forwarding dan lain-lain) untuk mencapai keselamatan dan paket penghalaan yang dikehendaki boleh dicapai dengan menggunakan firewall adat aplikasi. Penggunaan modul ini perlu dikhaskan untuk pengguna lanjutan sahaja.';
