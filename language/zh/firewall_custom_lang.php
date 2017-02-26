e4b794a3d52f99425ca678e17db004032f36893f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:43:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = '自定义防火墙应用程序提供了一个低级别的工具来配置先进的防火墙规则。';
+$lang['firewall_custom_app_name'] = '自定义防火墙';
+$lang['firewall_custom_command_is_not_permitted'] = '命令是不允许的。';
+$lang['firewall_custom_description_invalid'] = '说明是无效的。';
+$lang['firewall_custom_firewall_rule_invalid'] = '防火墙规则是无效的。';
+$lang['firewall_custom_help_dragging'] = '您的自定义防火墙规则的顺序很重要 - 单击并表向上或向下拖动规则来更改顺序。';
+$lang['firewall_custom_help_iptables_constant'] = '$ IPTABLES是iptables命令，其中包括标志，以防止文件锁定问题一个常数。 可能时，请使用此常量！';
+$lang['firewall_custom_restart_required'] = '你的规则的顺序可能发生了变化 - 需要一个防火墙重新启动。';
+$lang['firewall_custom_summary'] = '要添加允许定制防火墙规则。 在高级或高度定制的网络环境，先进的防火墙规则除了不能使用现有的防火墙应用程序被添加（来电，DMZ，MultiWAN，端口转发等），以达到所需的安全和数据包的路由可以通过自定义防火墙来实现应用程序。 使用这一模块可应预留只有高级用户。';
