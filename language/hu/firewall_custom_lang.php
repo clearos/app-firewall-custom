2db6bf77e7d6b41cbda00304401c8677be082731
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:45:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'Az egyéni tűzfal app nyújt alacsony szintű eszközzel konfigurálhatók fejlett tűzfal szabályokat.';
+$lang['firewall_custom_app_name'] = 'egyéni tűzfal';
+$lang['firewall_custom_command_is_not_permitted'] = 'Parancs nem engedélyezett.';
+$lang['firewall_custom_description_invalid'] = 'Leírás érvénytelen.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'Tűzfal szabály érvénytelen.';
+$lang['firewall_custom_help_dragging'] = 'Az, hogy az egyéni tűzfal szabályok fontos - kattintson és húzza a szabályt felfelé vagy lefelé a táblázatban, hogy módosítsa a sorrendben.';
+$lang['firewall_custom_help_iptables_constant'] = '$ Iptables állandó az iptables parancs, amely magában foglalja zászlók megelőzésére fájlzárolási kérdéseket. Kérjük, használja ezt az állandót, ha lehetséges!';
+$lang['firewall_custom_restart_required'] = 'Az, hogy a szabályok megváltoztak - a tűzfal szükség újraindításra.';
+$lang['firewall_custom_summary'] = 'Lehetővé teszi, hogy személyre szabott tűzfal szabályokat ki kell egészíteni. Előrehaladott vagy nagyon egyedi hálózati környezetben, a mellett a fejlett tűzfal szabályok, amelyeket nem lehet hozzá, hogy a rendelkezésre álló tűzfal alkalmazásokat (bejövő, DMZ, MultiWAN, Port Forwarding stb), hogy elérjék a kívánt biztonsági és a csomag routing alkalmazásával valósítható meg az egyéni tűzfal app. Használja ezt a modult kell fenntartani csak haladó felhasználók számára.';
