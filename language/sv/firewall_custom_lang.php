6d4e1cf8b53c3160e6371c2e2544bbd7a579fb50
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:51:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sv/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'Custom Firewall app ger ett verktyg med låg nivå för att konfigurera avancerade brandväggsregler.';
+$lang['firewall_custom_app_name'] = 'anpassad brandvägg';
+$lang['firewall_custom_command_is_not_permitted'] = 'Kommandot är inte tillåtet.';
+$lang['firewall_custom_description_invalid'] = 'Beskrivning är ogiltig.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'Brandväggsregel är ogiltig.';
+$lang['firewall_custom_help_dragging'] = 'Ordningen på dina egna brandväggsregler är viktigt - klicka och dra en regel uppåt eller nedåt i tabellen för att ändra ordningen.';
+$lang['firewall_custom_help_iptables_constant'] = '$ IPTABLES är en konstant för kommandot iptables som innehåller flaggor för att förhindra fillåsning frågor. Använd denna konstant när det är möjligt!';
+$lang['firewall_custom_restart_required'] = 'Ordningen på dina regler kan ha ändrats - en brandvägg omstart krävs.';
+$lang['firewall_custom_summary'] = 'Tillåter anpassade brandväggsregler som skall tillsättas. I avancerade eller specialanpassade nätverksmiljöer, tillsättning av avancerade brandväggsregler som inte kan tillsättas med hjälp av tillgängliga brandvägg program (Inkommande, DMZ, MultiWAN, Port Forwarding etc.) för att uppnå önskad säkerhet och paket routing kan åstadkommas med hjälp av anpassade brandväggs app. Användning av denna modul bör reserveras för enbart avancerade användare.';
