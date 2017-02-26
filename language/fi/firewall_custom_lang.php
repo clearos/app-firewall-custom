600ba7b13fb8b9d4bcfe19eb0ccc2d99fd7f55af
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:43:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'Custom Palomuuri sovellus tarjoaa matalan tason työkalu määrittää kehittyneen palomuurin sääntöjä.';
+$lang['firewall_custom_app_name'] = 'Custom Firewall';
+$lang['firewall_custom_command_is_not_permitted'] = 'Komento ei ole sallittua.';
+$lang['firewall_custom_description_invalid'] = 'Kuvaus on virheellinen.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'Palomuurisääntö on virheellinen.';
+$lang['firewall_custom_help_dragging'] = 'Järjestys muokatun palomuurin sääntöjä on tärkeää - valita ja vetää pääsääntöisesti ylös- tai alaspäin taulukossa muuttaa järjestystä.';
+$lang['firewall_custom_help_iptables_constant'] = '$ Iptables on vakio iptables sisältävän komentorivin liput estää tiedostojen lukitus kysymyksiä. Käytä tätä vakiona, kun mahdollista!';
+$lang['firewall_custom_restart_required'] = 'Järjestys säännöistäsi saattanut muuttua - palomuurin uudelleenkäynnistys tarvitaan.';
+$lang['firewall_custom_summary'] = 'Mahdollistaa räätälöityjä palomuurisäännöt lisättävä. Kehittyneissä tai hyvin räätälöityjä verkkoympäristöissä, lisäämällä kehittyneen palomuurin sääntöjä, joita ei voida lisätä käytettävissä olevien palomuurin sovelluksia (Saapuva, ​​DMZ, MultiWAN, porttien jne.) Halutun turvallisuus- ja pakettien reititys voidaan toteuttaa käyttämällä mukautettua palomuuri sovellus. Käyttämällä tätä moduulia olisi varattava vain kokeneille käyttäjille.';
