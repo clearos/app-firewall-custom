b8e5eca7fdfbbff7fe598a05f2458c77eb75f435
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:44:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/de/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'Die Custom-Firewall App bietet eine Low-Level-Tool erweiterte Firewall-Regeln zu konfigurieren.';
+$lang['firewall_custom_app_name'] = 'Benutzerdefinierte Firewall';
+$lang['firewall_custom_command_is_not_permitted'] = 'Befehl ist nicht zulässig.';
+$lang['firewall_custom_description_invalid'] = 'Beschreibung ist ungültig.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'Firewall-Regel ist ungültig.';
+$lang['firewall_custom_help_dragging'] = 'Die Reihenfolge der benutzerdefinierten Firewall-Regeln ist wichtig - klicken und eine Regel nach oben oder unten in der Tabelle ziehen die Reihenfolge zu ändern.';
+$lang['firewall_custom_help_iptables_constant'] = '$ IPTABLES ist eine Konstante für den iptables-Befehl, der Flaggen zu verhindern Dateisperrung Fragen enthält. Bitte verwenden Sie diese Konstante, wenn möglich!';
+$lang['firewall_custom_restart_required'] = 'Die Reihenfolge der Regeln geändert haben - eine Firewall Neustart erforderlich ist.';
+$lang['firewall_custom_summary'] = 'Ermöglicht individuelle Firewall-Regeln hinzugefügt werden. In fortgeschrittenen oder hochNetzWerkUmgebungen angepasst, die Zugabe von erweiterten Firewall-Regeln, die nicht die verfügbaren Firewall-Anwendungen hinzugefügt werden können, unter Verwendung von (Incoming, DMZ, MultiWAN, Port Forwarding etc.) die gewünschte Sicherheit und Paketweiterleitung zu erreichen, kann mit der benutzerdefinierten Firewall werden erreicht App. Die Nutzung dieses Moduls sollte nur für fortgeschrittene Anwender reserviert werden.';
