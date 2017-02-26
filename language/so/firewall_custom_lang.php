e25cac1e13a09d58815def6184898e27cc333f8b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:50:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'The app Custom Brannmur siisaa qalab heerka hoose ah reserved xeerarka brannmur sare.';
+$lang['firewall_custom_app_name'] = 'Brannmur Custom';
+$lang['firewall_custom_command_is_not_permitted'] = 'Taliska lama ogola.';
+$lang['firewall_custom_description_invalid'] = 'Description waa baadil.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'xeerka Brannmur waa baadil.';
+$lang['firewall_custom_help_dragging'] = 'Si The xeerarka brannmur caadadii aad muhiim ah - guji iyo jiidi caadiga ah kor ama hoos in miiska ay u bedesho amarka.';
+$lang['firewall_custom_help_iptables_constant'] = '$ IPTABLES waa joogto ah loogu dhigay amarka iptables in ka mid ah calanka si looga hortago arrimaha file dareysa. Fadlan isticmaal joogto ah marka ay suurto gal!';
+$lang['firewall_custom_restart_required'] = 'Si The xeerarka aad isbedeli karta - qeybtii a brannmur ayaa loo baahan yahay.';
+$lang['firewall_custom_summary'] = 'Kuu ogolaanayaa xeerarka brannmur habka in lagu daro. In jawi xirka sare ama heer sare kartoo, Intaa waxaa dheer ee xeerarka brannmur sare oo aan lagu dari karaa iyadoo la isticmaalayo apps brannmur la heli karo (soo socda, DMZ, MultiWAN, Port gudbinta iwm) si ay u gaaraan amniga iyo baakadda isdaba la doonayo in lagu dhammeeyo karaa iyadoo la isticmaalayo brannmur caadadii app. Isticmaalka module this waa in loo isticmaala sare oo keliya.';
