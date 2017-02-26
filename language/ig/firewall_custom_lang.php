f545f0140113370013c0316fb0a5d9a7f262f0af
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:45:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'The Omenala Firewall ngwa na-enye a ala-larịị ngwá ọrụ iji hazi elu firewall iwu.';
+$lang['firewall_custom_app_name'] = 'Custom Firewall';
+$lang['firewall_custom_command_is_not_permitted'] = '-Iwu adighi-ekwe.';
+$lang['firewall_custom_description_invalid'] = 'Description bụ ghara ịdị irè.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'Firewall na-achị bụ ghara ịdị irè.';
+$lang['firewall_custom_help_dragging'] = 'Iji nke gị omenala firewall iwu dị mkpa - pịa na ịdọrọ a na-achị elu ma ọ bụ ala na table ịgbanwe iji.';
+$lang['firewall_custom_help_iptables_constant'] = '$ IPTABLES dị mgbe nile maka iptables iwu na-agụnye flags iji gbochie file kpochidoro okwu. Biko jiri nke a mgbe nile mgbe o kwere omume!';
+$lang['firewall_custom_restart_required'] = 'Iji nke gị iwu nwere ike gbanwere - a firewall Malitegharịa ekwentị a chọrọ.';
+$lang['firewall_custom_summary'] = '-Enye ohere ahaziri firewall iwu na-kwukwara. Na elu ma ọ bụ nke ukwuu na ahaziri ụka n&#39;Ịntanet gburugburu, na mgbakwunye na nke elu firewall iwu a na-apụghị kwukwara na iji dị firewall égwu (abata na, DMZ, MultiWAN, Port Forwarding wdg) iji nweta ihe ndị ha chọrọ nche na Packet nsochi usoro pụrụ ime nke a site na iji omenala firewall ngwa. Jiri nke a modul ga-debeere elu ọrụ nanị.';
