474114681cff876ebbd80df421bd7d0058be6470
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:43:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hr/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'Aplikacija Custom Firewall pruža funkcije niske razine za konfiguriranje napredne firewall pravila.';
+$lang['firewall_custom_app_name'] = 'Prilagođena Firewall';
+$lang['firewall_custom_command_is_not_permitted'] = 'Naredba nije dozvoljeno.';
+$lang['firewall_custom_description_invalid'] = 'Opis nije valjan.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'Firewall pravilo je nevažeći.';
+$lang['firewall_custom_help_dragging'] = 'Redoslijed prilagođenim firewall pravila važna - kliknite i povucite pravilo prema gore ili dolje u tablici promijeniti redoslijed.';
+$lang['firewall_custom_help_iptables_constant'] = '$ Iptables je konstanta za naredbu iptables s upaljenim koja uključuje zastave kako bi se spriječilo datoteka zaključavanje pitanja. Molimo koristite ovu konstantu kada je to moguće!';
+$lang['firewall_custom_restart_required'] = 'Redoslijed svojih pravila možda se promijenila - potrebno je vatrozid ponovno pokretanje.';
+$lang['firewall_custom_summary'] = 'Omogućuje prilagođene firewall pravila biti dodan. U naprednim ili visoko prilagođenih mrežnih okruženja, dodavanje naprednih firewall pravila koja se ne može dodati pomoću dostupnih aplikacija vatrozida (Incoming, DMZ, MultiWAN, Port Forwarding) kako bi se postigao željeni sigurnosti i paketa usmjeravanje se može postići pomoću prilagođenog vatrozid app. Korištenje ovog modula bi trebao biti rezerviran samo za napredne korisnike.';
