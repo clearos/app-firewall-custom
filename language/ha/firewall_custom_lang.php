a21de81180b9e5ad1159ab7eeb4c0c5f74c8bbbc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:44:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'The Custom Firewall app bayar da wani low-matakin kayan aiki don daidaita m Firewall dokoki.';
+$lang['firewall_custom_app_name'] = 'Custom Firewall';
+$lang['firewall_custom_command_is_not_permitted'] = 'Umurnin da aka ba izini.';
+$lang['firewall_custom_description_invalid'] = 'Description ba daidai ba ne.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'Firewall mulki ba daidai ba ne.';
+$lang['firewall_custom_help_dragging'] = 'The tsari na al&#39;ada Firewall dokoki da muhimmanci - danna kuma ja mai mulkin sama ko ƙasa a kan tebur a canja tsari.';
+$lang['firewall_custom_help_iptables_constant'] = '$ Iptables ne akai ga iptables umurnin da ya hada da flags su hana file kulle al&#39;amurran da suka shafi. Don Allah yi amfani da wannan m lokacin zai yiwu!';
+$lang['firewall_custom_restart_required'] = 'The tsari na dokoki iya canja - a Firewall sake kunnawa ake bukata.';
+$lang['firewall_custom_summary'] = 'Bayar Musamman Firewall dokoki da za a kara da cewa. A ci-gaba ko sosai musamman sadarwar yanayin, Bugu da kari na ci-gaba Firewall dokoki da ba za a iya kara yin amfani da samuwa Firewall apps (shigowa, DMZ, MultiWAN, Port Mikawa da dai sauransu) to kai da ake so tsaro da fakiti bayar da kwatance da za a iya cika ta yin amfani da custom Firewall app. Amfani da wannan a koyaushe ya kamata a tanada m masu amfani kawai.';
