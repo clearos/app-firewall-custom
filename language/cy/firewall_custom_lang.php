4f94d5be0b8899712279f50bea149df768a5f04b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:52:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'Mae&#39;r app Custom Firewall yn darparu offeryn lefel isel i ffurfweddu wal dân rheolau uwch.';
+$lang['firewall_custom_app_name'] = 'Firewall Custom';
+$lang['firewall_custom_command_is_not_permitted'] = 'Ni chaniateir Reoli.';
+$lang['firewall_custom_description_invalid'] = 'Disgrifiad yn annilys.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'rheol Firewall yn annilys.';
+$lang['firewall_custom_help_dragging'] = 'Mae trefn y eich rheolau firewall arfer yn bwysig - cliciwch a llusgo rheol i fyny neu i lawr yn y tabl i newid trefn.';
+$lang['firewall_custom_help_iptables_constant'] = '$ Iptables yn gyson am y gorchymyn iptables sy&#39;n cynnwys baneri i atal materion ffeiliau cloi. Defnyddiwch gyson hwn pan bosibl!';
+$lang['firewall_custom_restart_required'] = 'Efallai y bydd y drefn eich rheolau wedi newid - mae angen ailgychwyn wal dân.';
+$lang['firewall_custom_summary'] = 'Yn caniatáu rheolau firewall customized i&#39;w hychwanegu. Mewn amgylcheddau rhwydweithio datblygedig neu customized iawn, ychwanegu rheolau firewall uwch na ellir ei hychwanegu gan ddefnyddio&#39;r apps firewall sydd ar gael (sy&#39;n dod i mewn, DMZ, MultiWAN, Port Anfon ati) er mwyn cyflawni&#39;r diogelwch a paced llwybro ddymunir gellir ei gyflawni drwy ddefnyddio&#39;r firewall arferiad app. Dylai defnyddio&#39;r modiwl hwn yn cael ei gadw ar gyfer defnyddwyr uwch yn unig.';
