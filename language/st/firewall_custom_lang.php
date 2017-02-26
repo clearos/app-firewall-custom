4f8c2d24b423a708cfe4b3f61dc9c9a64b3bfd4a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:50:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'The app e Custom Pula Tsa Lehlohonolo fana ka sesebelisoa tlase haholo ho isa configure melawana fayawolo e tsoetseng pele.';
+$lang['firewall_custom_app_name'] = 'Custom Pula Tsa Lehlohonolo';
+$lang['firewall_custom_command_is_not_permitted'] = 'Taelo e sa lumelloa.';
+$lang['firewall_custom_description_invalid'] = 'Tlhaloso ha o sebetse.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'puso Pula Tsa Lehlohonolo ha o sebetse.';
+$lang['firewall_custom_help_dragging'] = 'Taelo oa phatla ea hao melawana fayawolo moetlo ke habohlokoa - tobetsa le hula busa hodimo kapa tlase a tafole a ho fetola taelo ea.';
+$lang['firewall_custom_help_iptables_constant'] = '$ IPTABLES ke kamehla bakeng sa iptables taelo e kenyeletsang lifolakha ho thibela litokollo faele ho notlela. Kopo sebelisa kamehla sena ha ho khoneha!';
+$lang['firewall_custom_restart_required'] = 'Taelo ea melao ea hao ka &#39;na eaba ba fetoha - ho qala botjha fayawolo ho hlokahala.';
+$lang['firewall_custom_summary'] = 'Dumella melawana fayawolo customized ho phaella. A dibaka e tsoetseng pele kapa e tlotloang customized ho etsa metsoalle, ho eketsoa melao fayawolo e tsoetseng pele tse ke keng tsa phaella ho sebedisa fumaneha ditiriso fayawolo (tlang, DMZ, MultiWAN, Port fetisetse joalo-joalo) ho fihlela a lakatsa tšireletseho le pakete routing ka finyelloa sebelisa fayawolo moetlo app e. Tshebediso ya mojuleng sena se lokela ho boloketsoe basebedisi e tsoetseng pele feela.';
