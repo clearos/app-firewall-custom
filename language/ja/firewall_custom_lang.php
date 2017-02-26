2feb7c01903c95b7ffd7b23b70c015ab0ef40632
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:46:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'カスタムファイアウォールアプリは、高度なファイアウォールルールを設定するには、低レベルのツールを提供します。';
+$lang['firewall_custom_app_name'] = 'カスタムファイアウォール';
+$lang['firewall_custom_command_is_not_permitted'] = 'コマンドが許可されていません。';
+$lang['firewall_custom_description_invalid'] = '説明が無効です。';
+$lang['firewall_custom_firewall_rule_invalid'] = 'ファイアウォールルールは無効です。';
+$lang['firewall_custom_help_dragging'] = 'カスタムファイアウォールルールの順序は重要です - クリックして、順序を変更する場合は、表の上または下のルールをドラッグします。';
+$lang['firewall_custom_help_iptables_constant'] = '$ IPTABLESは、ファイルロックの問題を防止するためのフラグを含んでiptablesコマンドに対して一定です。 可能な場合は、この定数を使用してください！';
+$lang['firewall_custom_restart_required'] = 'あなたのルールの順序が変更されている可能性 - ファイアウォールの再起動が必要です。';
+$lang['firewall_custom_summary'] = 'カスタマイズされたファイアウォールルールを追加することができます。 高度または高度にカスタマイズされたネットワーク環境では、必要なセキュリティおよびパケットルーティングを実現するために利用可能なファイアウォールのアプリケーション（など着信、DMZ、MultiWAN、ポートフォワーディング）を使用して追加することができない高度なファイアウォールルールの追加は、カスタムファイアウォールを使用して達成することができますアプリ。 このモジュールの使用は、上級ユーザーのみに予約する必要があります。';
