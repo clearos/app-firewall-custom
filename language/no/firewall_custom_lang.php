11acea3e79ea074691fb263be566bc46008f0c70
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:48:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/no/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'The Custom brannmur app gir et lavt nivå verktøy for å konfigurere avanserte brannmurregler.';
+$lang['firewall_custom_app_name'] = 'Custom Firewall';
+$lang['firewall_custom_command_is_not_permitted'] = 'Command er ikke tillatt.';
+$lang['firewall_custom_description_invalid'] = 'Beskrivelse er ugyldig.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'Brannmurregel er ugyldig.';
+$lang['firewall_custom_help_dragging'] = 'Rekkefølgen på egendefinerte brannmurregler er viktig - klikk og dra en regel opp eller ned i tabellen for å endre rekkefølgen.';
+$lang['firewall_custom_help_iptables_constant'] = '$ Iptables er en konstant for iptables-kommandoen som inkluderer flagg for å hindre fillåsing problemer. Vennligst bruk dette konstant når det er mulig!';
+$lang['firewall_custom_restart_required'] = 'Rekkefølgen på reglene kan ha endret seg - en brannmur omstart kreves.';
+$lang['firewall_custom_summary'] = 'Lar tilpassede brannmurregler som skal legges. I avanserte eller svært tilpassede nettverksmiljøer, tillegg av avanserte brannmurregler som ikke kan legges til ved å bruke de tilgjengelige brannmur apps (innkommende, DMZ, MultiWAN, Port Forwarding etc.) for å oppnå ønsket sikkerhet og pakke ruting kan oppnås ved hjelp av tilpassede brannmur app. Bruk av denne modulen skal reserveres for avanserte brukere.';
