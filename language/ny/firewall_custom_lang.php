98e97e78dc1a930beafa268fe2f4e873b028124b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:42:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'The app Mwambo makhoma oteteza lili chida otsika mlingo kuti sintha malamulo makhoma oteteza patsogolo.';
+$lang['firewall_custom_app_name'] = 'mwambo makhoma oteteza';
+$lang['firewall_custom_command_is_not_permitted'] = 'Lamulo sikuloledwa.';
+$lang['firewall_custom_description_invalid'] = 'Kufotokozera ndi opanda pake.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'ulamuliro makhoma oteteza ndi opanda pake.';
+$lang['firewall_custom_help_dragging'] = 'Dongosolo la wanu malamulo makhoma oteteza mwambo n&#39;kofunika - alemba ndipo kokerani ulamuliro kapena pansi pa tebulo kusintha dongosolo.';
+$lang['firewall_custom_help_iptables_constant'] = '$ IPTABLES nthawi zonse kwa iptables lamulo monga mbendera kuteteza nkhani file kutseka. Chonde ntchito zonse izi ngati n&#39;kotheka!';
+$lang['firewall_custom_restart_required'] = 'Dongosolo la malamulo anu ayenera kusinthidwa - ndi kuyambitsanso makhoma oteteza chofunika.';
+$lang['firewall_custom_summary'] = 'Amalola malamulo makhoma oteteza sinthidwa mwamakonda kuti zidzawonjezedwa. Mapangidwe apamwamba kapena kwambiri makonda Intaneti Komanso malamulo makhoma oteteza zapamwamba kuti sangathe zidzawonjezedwa ntchito zilipo mapulogalamu makhoma oteteza (Zofika, DMZ, MultiWAN, Port kutumiza etc.) kukwaniritsa kufunika chitetezo ndi paketi yolozera Mukhoza kuchita ntchito makhoma oteteza mwambo app. Ntchito gawo ayenera kuti asungidwe owerenga zokhwima zokhazokha.';
