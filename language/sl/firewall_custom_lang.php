27105eff6ff6bb33eb898555e6d6ef4e03ac72cd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:50:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sl/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'Aplikacija Firewall meri zagotavlja orodje nizko stopnjo na konfiguracijo naprednih pravil požarnega zidu.';
+$lang['firewall_custom_app_name'] = 'Firewall meri';
+$lang['firewall_custom_command_is_not_permitted'] = 'Ukaz ni dovoljen.';
+$lang['firewall_custom_description_invalid'] = 'Opis je neveljavna.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'Firewall pravilo je neveljavna.';
+$lang['firewall_custom_help_dragging'] = 'Vrstni red vaših pravil meri požarnega zidu je pomembna - kliknite in povlecite pravilo gor ali dol v tabeli, da spremenite vrstni red.';
+$lang['firewall_custom_help_iptables_constant'] = '$ Iptables je konstanta za ukaz iptables, ki vključuje zastavice za preprečevanje datotek zaklepanje vprašanja. Uporabite to konstanto kadar je to mogoče!';
+$lang['firewall_custom_restart_required'] = 'Vrstni red vaših pravil lahko spremenila - je potreben požarni zid ponovni zagon.';
+$lang['firewall_custom_summary'] = 'Omogoča prilagojene pravila požarnega zidu je treba dodati. V razvitih ali zelo prilagojenih omrežnih okolij, dodajanje naprednih pravil požarnega zidu, ki jih ni mogoče dodati z uporabo razpoložljivih firewall aplikacije za (Incoming, DMZ, MultiWAN, Port Forwarding itd), da bi dosegli želeno varnost in paketno usmerjanje je mogoče doseči z uporabo požarnega zidu po meri app. Uporaba tega modula naj bi bile omejene samo naprednim uporabnikom.';
