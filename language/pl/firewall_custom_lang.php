432eecc0ddba9c2c9383859f31a2368bceff33d1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:49:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pl/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'Aplikacja klienta Firewall zapewnia narzędzia niskiego poziomu konfiguracji zaawansowanych reguł zapory.';
+$lang['firewall_custom_app_name'] = 'Zapora niestandardowe';
+$lang['firewall_custom_command_is_not_permitted'] = 'Polecenie nie jest dozwolone.';
+$lang['firewall_custom_description_invalid'] = 'Opis jest nieprawidłowy.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'reguła zapory jest nieprawidłowy.';
+$lang['firewall_custom_help_dragging'] = 'Kolejność niestandardowych reguł zapory jest ważne - kliknij i przeciągnij reguły górę lub w dół w tabeli, aby zmienić kolejność.';
+$lang['firewall_custom_help_iptables_constant'] = '$ IPTABLES jest stała dla polecenia iptables, który zawiera flagi Aby uniknąć problemów blokowania plików. Proszę używać tej stałej, jeśli to możliwe!';
+$lang['firewall_custom_restart_required'] = 'Kolejność reguł uległy zmianie - wymagany jest restart firewalla.';
+$lang['firewall_custom_summary'] = 'Pozwala niestandardowe reguły zapory sieciowej do dodania. W zaawansowanych lub wysoce spersonalizowanych środowisk sieciowych, dodawanie zaawansowanych reguł zapory, które nie mogą być dodawane za pomocą dostępnych aplikacji zapory (przychodzące, DMZ, MultiWAN, Port Forwarding etc.) w celu osiągnięcia pożądanego bezpieczeństwa i pakietów routingu może być dokonane przy użyciu niestandardowego zaporę app. Zastosowanie tego modułu powinno być zarezerwowane tylko dla zaawansowanych użytkowników.';
