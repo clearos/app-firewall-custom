a47a0c0bc69a50237833a842ecd5f70795fb1ced
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:46:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'A app consuetudinem Antivirus tool ut suggero a low-level Advanced Firewall normas conforment.';
+$lang['firewall_custom_app_name'] = 'Custom Firewall';
+$lang['firewall_custom_command_is_not_permitted'] = 'Imperium non licet.';
+$lang['firewall_custom_description_invalid'] = 'Description est invalidum.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'Firewall Pro regula non valet.';
+$lang['firewall_custom_help_dragging'] = 'Ordo regit mos Firewall refert - click trahunt regula uel immutare ordinem in tabula.';
+$lang['firewall_custom_help_iptables_constant'] = '$ Iptables constans est, quod habet mandatum iptables in papyrione ne file obfirmatis exitus. Quaeso uti semper fieri potest;';
+$lang['firewall_custom_restart_required'] = 'Ut iura ordo mutatus - sileo a firewall requiritur.';
+$lang['firewall_custom_summary'] = 'Permittit Firewall Pro Customized legibus addidit. In environments networking Advanced vel amet illum, aetatis adiectione quod non potest addidit praecepta Firewall Pro Agnitum apps usura available (invenientes, DMZ, MultiWAN, portum transmittere etc), ad consequi desideravit in securitatem et fasciculum fuso potest fieri per consuetudinem Firewall App. Provectus users tantum usu moduli vacaret.';
