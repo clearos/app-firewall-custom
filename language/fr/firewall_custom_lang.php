500195460cb8c72c5f53dd5e92daecd94fe2c43a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:44:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fr/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'L&#39;application personnalisée Firewall fournit un outil de bas niveau pour configurer des règles de pare-feu avancées.';
+$lang['firewall_custom_app_name'] = 'Pare-feu personnalisé';
+$lang['firewall_custom_command_is_not_permitted'] = 'Commande est pas autorisée.';
+$lang['firewall_custom_description_invalid'] = 'La description est invalide.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'règle de pare-feu est invalide.';
+$lang['firewall_custom_help_dragging'] = 'L&#39;ordre de vos règles de pare-feu personnalisé est important - cliquez et faites glisser une règle vers le haut ou vers le bas dans le tableau pour modifier l&#39;ordre.';
+$lang['firewall_custom_help_iptables_constant'] = '$ IPTABLES est une constante pour la commande iptables qui comprend des drapeaux pour prévenir les problèmes de verrouillage de fichiers. S&#39;il vous plaît utiliser cette constante lorsque cela est possible!';
+$lang['firewall_custom_restart_required'] = 'L&#39;ordre de vos règles peut avoir changé - un redémarrage pare-feu est nécessaire.';
+$lang['firewall_custom_summary'] = 'Permet à des règles de pare-feu personnalisées à ajouter. Dans les environnements de réseau avancées ou hautement personnalisés, l&#39;ajout de règles de pare-feu avancées qui ne peut être ajouté en utilisant les applications de pare-feu disponibles (Incoming, DMZ, MultiWAN, Port Forwarding etc.) pour parvenir à la sécurité et le routage des paquets souhaité peut être accompli en utilisant le pare-feu personnalisé app. L&#39;utilisation de ce module devrait être réservé aux seuls utilisateurs avancés.';
