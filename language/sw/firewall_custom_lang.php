ea019abad1bdc9e9ce84b274481d34d6dcc4dbc1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:50:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'programu Desturi Firewall hutoa chombo ngazi ya chini ya configure firewall sheria juu.';
+$lang['firewall_custom_app_name'] = 'desturi Firewall';
+$lang['firewall_custom_command_is_not_permitted'] = 'Amri hairuhusiwi.';
+$lang['firewall_custom_description_invalid'] = 'Maelezo ni batili.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'utawala Firewall ni batili.';
+$lang['firewall_custom_help_dragging'] = 'utaratibu wa sheria yako firewall desturi ni muhimu - click na Drag utawala juu au chini katika meza kubadili utaratibu.';
+$lang['firewall_custom_help_iptables_constant'] = '$ Iptables ni mara kwa mara kwa iptables amri kuwa ni pamoja na bendera ya kuzuia masuala ya faili kuifungia. Tafadhali kutumia mara kwa mara hii wakati inawezekana!';
+$lang['firewall_custom_restart_required'] = 'utaratibu wa sheria yako inaweza kuwa iliyopita - upya firewall inahitajika.';
+$lang['firewall_custom_summary'] = 'Inaruhusu sheria firewall customized kuongezwa. Katika mazingira ya juu au umeboreshwa sana mitandao, nyongeza ya sheria firewall juu kwamba haiwezi kuongezwa kwa kutumia inapatikana programu firewall (zinazoingia, DMZ, MultiWAN, Port Usambazaji nk) ili kufikia taka usalama na pakiti routing inaweza kukamilika kwa kutumia firewall desturi programu. Matumizi ya moduli hii lazima akiba kwa watumiaji wa juu tu.';
