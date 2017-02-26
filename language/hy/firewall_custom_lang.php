b71bfbf8b837ebdc90d9171d40b3ec35f2478ee0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:41:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'The Custom Firewall ծրագիրը ապահովում է ցածր մակարդակի գործիք կարգավորել առաջադեմ firewall կանոնները:';
+$lang['firewall_custom_app_name'] = 'Custom Firewall';
+$lang['firewall_custom_command_is_not_permitted'] = 'Command չի թույլատրվում:';
+$lang['firewall_custom_description_invalid'] = 'Նկարագրություն անվավեր է:';
+$lang['firewall_custom_firewall_rule_invalid'] = 'Firewall կանոն է անվավեր:';
+$lang['firewall_custom_help_dragging'] = 'Կարգը ձեր մաքսային firewall կանոնների կարեւոր է, - սեղմեք եւ քաշել մի կանոն վեր կամ վար է սեղանի փոխել կարգը:';
+$lang['firewall_custom_help_iptables_constant'] = '$ IPTABLES է անընդհատ է iptables հրամանատարության, որը ներառում է դրոշները կանխելու ֆայլի locking հարցեր: Խնդրում ենք օգտագործել այս մշտականությունը, երբ հնարավոր է!';
+$lang['firewall_custom_restart_required'] = 'Կարգը ձեր կանոնների կարող են փոխվել, մի firewall վերսկսել է պահանջվում.';
+$lang['firewall_custom_summary'] = 'Թույլ Պատվերով firewall կանոնները պետք է ավելացվի: Ընդլայնված կամ բարձր անհատական ​​ցանցային միջավայրում, ժամանակը առաջադեմ firewall կանոնների, որոնք չեն կարող ավելացվել, օգտագործելով առկա firewall apps (Ներգնա, DMZ, MultiWAN, Պորտ վերահասցեավորում եւ այլն), ինչպես նաեւ հասնել ցանկալի անվտանգության եւ փաթեթային routing կարելի է հասնել օգտագործելով հարմարեցված firewall ծրագիրը: Օգտագործեք այս մոդուլի պետք է պաշտպանված է զարգացած օգտվողների միայն.';
