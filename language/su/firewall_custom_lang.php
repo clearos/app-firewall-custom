1131f6f66432a98ec608911c2128eb971d1b15e4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:50:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'Nu Adat firewall app nyadiakeun pakakas-tingkat low pikeun ngonpigurasikeun aturan firewall maju.';
+$lang['firewall_custom_app_name'] = 'firewall adat';
+$lang['firewall_custom_command_is_not_permitted'] = 'Paréntah teu diijinkeun.';
+$lang['firewall_custom_description_invalid'] = 'Description téh sah.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'aturan firewall anu sah.';
+$lang['firewall_custom_help_dragging'] = 'Urutan aturan firewall adat Anjeun penting - klik anjeun sarta sered aturan luhur atawa ka handap dina tabel pikeun ngarobah urutan.';
+$lang['firewall_custom_help_iptables_constant'] = '$ IPTABLES nyaéta konstanta pikeun paréntah iptables nu ngawengku umbul pikeun nyegah file tumerapna isu. Mangga pigunakeun konstan ieu lamun mungkin!';
+$lang['firewall_custom_restart_required'] = 'Urutan aturan anjeun bisa geus robah - a balikan deui firewall anu diperlukeun.';
+$lang['firewall_custom_summary'] = 'Ngamungkinkeun aturan firewall ngaropéa bisa ditambahkeun. Dina lingkungan jaringan maju atawa kacida ngaropéa, ditambah aturan firewall maju nu teu bisa ditambahkeun ngagunakeun aktip firewall sadia (asup, DMZ, MultiWAN, Port Neraskeun jsb) pikeun ngahontal kaamanan jeung packet routing dipikahoyong bisa dilakonan ngagunakeun firewall adat app. Pamakéan modul ieu kudu ditangtayungan pikeun pamaké maju wungkul.';
