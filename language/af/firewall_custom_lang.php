234ca928d7215a986ba38a39fd2ad9ec1a52fbc0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:41:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'Die Custom Firewall app bied &#39;n lae-vlak instrument om gevorderde firewall reëls instel.';
+$lang['firewall_custom_app_name'] = 'Custom Firewall';
+$lang['firewall_custom_command_is_not_permitted'] = 'Opdrag is nie toelaatbaar nie.';
+$lang['firewall_custom_description_invalid'] = 'Beskrywing is ongeldig.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'Firewall reël is ongeldig.';
+$lang['firewall_custom_help_dragging'] = 'Die orde van jou persoonlike firewall reëls is belangrik - kliek en sleep &#39;n reël op of af in die tabel om die orde te verander.';
+$lang['firewall_custom_help_iptables_constant'] = '$ Iptables is &#39;n konstante vir die iptables opdrag dat vlae lêer sluit kwessies te voorkom sluit. Gebruik asseblief hierdie konstante wanneer moontlik!';
+$lang['firewall_custom_restart_required'] = 'Die orde van jou reëls kan verander - &#39;n firewall herlaai word vereis.';
+$lang['firewall_custom_summary'] = 'Laat persoonlike firewall reëls toe te voeg. In gevorderde of hoogs persoonlike netwerk omgewings, die byvoeging van &#39;n gevorderde firewall reëls wat nie kan bygevoeg word deur gebruik te maak van die beskikbare firewall programme (inkomende, DMZ, MultiWAN, Port Forwarding ens) om die verlangde sekuriteit en pakkie routing bereik kan word bereik met behulp van die persoonlike firewall app. Die gebruik van hierdie module moet gereserveer word vir net gevorderde gebruikers.';
