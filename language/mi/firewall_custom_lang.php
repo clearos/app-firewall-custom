577e1065a6a23e9e559f4b62e0d27f18c85cb34e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:47:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'whakarato te taupānga Pātūahi Ritenga he taputapu taumata-iti ki te whirihora ture pātūahi matatau.';
+$lang['firewall_custom_app_name'] = 'Pātūahi ritenga';
+$lang['firewall_custom_command_is_not_permitted'] = 'e kore te Whakahaua tukua.';
+$lang['firewall_custom_description_invalid'] = 'He muhu Whakamārama.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'He muhu tikanga pātūahi.';
+$lang['firewall_custom_help_dragging'] = 'He mea nui te tikanga o koutou ture pātūahi ritenga - pāwhiri ka toia te tikanga ake ki raro ranei i te tepu ki te huri i te raupapa.';
+$lang['firewall_custom_help_iptables_constant'] = 'Ko te tamau no te whakahau iptables e ngā haki ki te ārai i ngā take kōnae maukati $ IPTABLES. whakamahi koa i tēnei taimau ka taea!';
+$lang['firewall_custom_restart_required'] = 'Kua puta ke te tikanga o koutou ture kia - hiahiatia he tīmata pātūahi te.';
+$lang['firewall_custom_summary'] = 'Āhei ture pātūahi taa ki te tāpiri. I roto i te taiao whatunga matatau tino whakaritea ranei, te tua o ngā ture pātūahi matatau e kore e taea te tāpiri mā te whakamahi i te taupānga pātūahi e wātea ana (Taumai, DMZ, MultiWAN, Port whakamua etc) ki te whakatutuki i te haumarutanga me te pākete ararere e hiahiatia e taea te rite te whakamahi i te pātūahi ritenga taupānga. kia waiho e whakamahi o tenei kōwae mō ngā kaiwhakamahi matatau anake.';
