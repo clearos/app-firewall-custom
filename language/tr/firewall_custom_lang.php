9c20a4bcd66cd570a467a675608a00de0cfdc164
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:51:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'Özel Güvenlik uygulaması gelişmiş bir güvenlik duvarı kurallarını yapılandırmak için düşük seviyeli aracı sağlar.';
+$lang['firewall_custom_app_name'] = 'özel Güvenlik Duvarı';
+$lang['firewall_custom_command_is_not_permitted'] = 'Komut izin verilmez.';
+$lang['firewall_custom_description_invalid'] = 'Açıklama geçersiz.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'Güvenlik duvarı kuralı geçersiz.';
+$lang['firewall_custom_help_dragging'] = 'Özel güvenlik duvarı kuralları sırası önemlidir - tıklayın ve sırasını değiştirmek için tabloda yukarı ya da aşağı bir kural sürükleyin.';
+$lang['firewall_custom_help_iptables_constant'] = '$ IPTABLES dosya kilitleme sorunları önlemek için bayraklar içeren iptables komutu için bir sabittir. Mümkün olduğunda bu sabit kullanın!';
+$lang['firewall_custom_restart_required'] = 'senin kuralların sırası değişmiş olabilir - bir güvenlik duvarı yeniden başlatılması gerekir.';
+$lang['firewall_custom_summary'] = 'Verir özelleştirilmiş güvenlik duvarı kuralları eklenecek. gelişmiş ya da oldukça özelleştirilmiş ağ ortamlarında, mevcut güvenlik duvarı uygulamaları kullanarak eklenemez gelişmiş bir güvenlik duvarı kurallarının eklenmesi (DMZ, MultiWAN, Port Forwarding vb Gelen) özel güvenlik duvarı kullanılarak yapılabilir istenen güvenlik ve paket yönlendirme elde etmek için app. Bu modülün kullanımı sadece ileri düzey kullanıcılar için saklanmalıdır.';
