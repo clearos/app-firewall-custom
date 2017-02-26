b81b9e7b40450efad99c165f6f9bb1d2fcd941be
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:53:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'Uhlelo lokusebenza Custom firewall inikeza ithuluzi eliphansi ukuze ulungiselele imithetho firewall eziphambili.';
+$lang['firewall_custom_app_name'] = 'Custom firewall';
+$lang['firewall_custom_command_is_not_permitted'] = 'Command akuvunyelwe.';
+$lang['firewall_custom_description_invalid'] = 'Incazelo ayivumelekile.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'Ukubusa firewall ayivumelekile.';
+$lang['firewall_custom_help_dragging'] = 'I-oda of kuwumkhuba wakho firewall imithetho kubalulekile - chofoza bese uhudula umthetho phezulu noma phansi etafuleni ukushintsha oda.';
+$lang['firewall_custom_help_iptables_constant'] = '$ IPTABLES owenziwa njalo ngoba iptables umyalo kuhlanganisa amafulegi ukuvimbela nezindaba ifayela locking. Sicela usebenzise le njalo lapho kungenzeka!';
+$lang['firewall_custom_restart_required'] = 'Kungenzeka Umyalelo imithetho yakho washintsha - ukuqalisa firewall liyadingeka.';
+$lang['firewall_custom_summary'] = 'Ivumela ezinkambo imithetho firewall okungamelanga kwengezwe. In networking environments eziphambili noma egcizelele kakhulu, ukwengeza imithetho firewall advanced okungatholakala ngeke wanezela ngokusebenzisa iyatholakala firewall zokusebenza (olungenayo, okungavunyelwe ukulwa kuyo, MultiWAN, Port Ukudlulisela njll) ukuze kuzuzwe oyifunayo ukulondeka futhi iphakethe umzila Kungafezani besebenzisa firewall siko app. Ukusebenzisa le module kufanele reserved kubasebenzisi eziphambili kuphela.';
