cac28b73a5ec0384146e588c464de1c936a5c026
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:43:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cs/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'Aplikace Custom Firewall poskytuje nástroje pro nízkoúrovňové ke konfiguraci pokročilých pravidel brány firewall.';
+$lang['firewall_custom_app_name'] = 'Custom Firewall';
+$lang['firewall_custom_command_is_not_permitted'] = 'Příkaz není dovoleno.';
+$lang['firewall_custom_description_invalid'] = 'Popis je neplatný.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'Firewall pravidlo je neplatný.';
+$lang['firewall_custom_help_dragging'] = 'Pořadí vašich vlastních pravidel firewallu je důležité - kliknutím a tažením pravidlo nahoru nebo dolů v tabulce změnit pořadí.';
+$lang['firewall_custom_help_iptables_constant'] = '$ IPTABLES je konstanta pro příkaz iptables, která zahrnuje vlajkami, aby se zabránilo problémy s zamykání souborů. Použijte prosím tento konstantu, pokud je to možné!';
+$lang['firewall_custom_restart_required'] = 'Pořadí vašich pravidel mohlo změnit - je nutné restartovat firewall.';
+$lang['firewall_custom_summary'] = 'Umožňuje vlastní pravidla firewallu, které se doplňují. Ve vyspělých a vysoce přizpůsobených síťových prostředích, přidání pokročilých pravidel brány firewall, které nemohou být přidány pomocí dostupných aplikací firewallu (příchozí, DMZ, MultiWAN, Port Forwarding atd.) Pro dosažení požadované zabezpečení a směrování paketů lze provést pomocí vlastního firewall aplikace. Použití tohoto modulu by mělo být vyhrazeno pouze pro pokročilé uživatele.';
