8ced3d9c679c0b9a998f93b3b1e184577c60fdae
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:45:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'The Custom Firewall app veitir lágmark-láréttur flötur tól til að stilla háþróaður reglur eldvegg.';
+$lang['firewall_custom_app_name'] = 'Custom Firewall';
+$lang['firewall_custom_command_is_not_permitted'] = 'Stjórn er ekki heimil.';
+$lang['firewall_custom_description_invalid'] = 'Lýsing er ógilt.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'Firewall reglan er ógilt.';
+$lang['firewall_custom_help_dragging'] = 'Röð af sérsniðnum reglum eldvegg er mikilvægt - smelltu og dragðu reglu upp eða niður í töflunni til að breyta röð.';
+$lang['firewall_custom_help_iptables_constant'] = '$ IPTABLES er fasti fyrir iptables skipun sem felur fánar til að koma í veg skrá læsa málefni. Vinsamlegast notið þetta fasti þegar mögulegt!';
+$lang['firewall_custom_restart_required'] = 'Röð reglunum kann að hafa breyst - eldveggur endurræsa er krafist.';
+$lang['firewall_custom_summary'] = 'Leyfir sérsniðin reglur eldvegg til að bæta við. Í ítarlegri eða mjög sérsniðna net umhverfi, the samlagning af háþróaður reglum eldvegg sem ekki er hægt að bæta með tiltækum eldvegg forrit (Incoming, DMZ, MultiWAN, Port Áframsending osfrv) til að ná tilætluðum öryggi og Packet vegvísun geta vera leikinn með því að nota sérsniðna eldvegg app. Notkun á þessari einingu ætti að vera frátekin fyrir háþróaður notandi aðeins.';
