7ca593edab595fa96361c6aa5d91e0c0074cac95
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:47:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'Ny Custom Firewall rindrambaiko dia manome ny ambany lenta fitaovana ny fampiendrehana mandroso fitsipika Firewall.';
+$lang['firewall_custom_app_name'] = 'Custom Firewall';
+$lang['firewall_custom_command_is_not_permitted'] = 'Command tsy navela.';
+$lang['firewall_custom_description_invalid'] = 'Description dia tsy manan-kery.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'Firewall fitsipika dia tsy manan-kery.';
+$lang['firewall_custom_help_dragging'] = 'Ny lamin &#39;ny fomba amam-panao Firewall fitsipika dia zava-dehibe - tsindrio sy hisintona ny fitsipika miakatra na midina eo amin&#39;ny latabatra mba hanova ny filaminana.';
+$lang['firewall_custom_help_iptables_constant'] = '$ IPTABLES dia tapaka ho an&#39;ny iptables didy izay ahitana ny saina mba hisorohana ny rakitra Fanidiana olana. Mba mampiasa izany foana, rehefa azo atao!';
+$lang['firewall_custom_restart_required'] = 'Ny lamin &#39;ny fitsipika Mety ho niova - iray Firewall Restart takiana.';
+$lang['firewall_custom_summary'] = 'Mamela namboarina Firewall fitsipika mba homena. Amin&#39;ny mandroso na tena namboarina tambajotra tontolo iainana, ny koa ny nandroso Firewall fitsipika izay tsy azo koa mampiasa ny misy Firewall fampiharana (Incoming, DMZ, MultiWAN, Port nanatitra sns) mba hahatratra ny naniry fiarovana sy ny fonosana tambazotra mety ho tanteraka amin&#39;ny fampiasana ny fomba amam-panao Firewall App. Fampiasana Module ity dia tokony hotehirizina ho amin&#39;ny mpampiasa mandroso ihany.';
