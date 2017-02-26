f512daa9dca9520c4a788455830b0799a92bcff1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:43:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'Kohandatud tulemüüri rakendus annab madala näitaja seadistada arenenud tulemüüri reeglid.';
+$lang['firewall_custom_app_name'] = 'Custom tulemüür';
+$lang['firewall_custom_command_is_not_permitted'] = 'Käsk ei ole lubatud.';
+$lang['firewall_custom_description_invalid'] = 'Kirjeldus ei sobi.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'Tulemüürireegel on kehtetu.';
+$lang['firewall_custom_help_dragging'] = 'Järjekorras oma kohandatud tulemüüri reeglid on oluline - klõpsa ja lohista reeglina üles või alla tabelis järjekorda muuta.';
+$lang['firewall_custom_help_iptables_constant'] = '$ Iptables on konstant iptables käsk, mis sisaldab lippu, et takistada failide lukustamise küsimusi. Palun kasutage seda pidevat, kui võimalik!';
+$lang['firewall_custom_restart_required'] = 'Järjekorras oma reeglid võivad olla muutunud - tulemüür on vajalik taaskäivitamine.';
+$lang['firewall_custom_summary'] = 'Võimaldab kohandatud tulemüüri reeglid lisada. Kaugelearenenud või väga kohandatud võrgustiku keskkonnas, lisaks täiustatud tulemüüri reeglid, mida ei saa lisada, kasutades olemasolevaid tulemüüri rakendusi (Saabuva, DMZ, MultiWAN, pordiedastusega jne), et saavutada soovitud turvalisuse ja pakettide marsruutimine on võimalik saavutada kasutades kohandatud tulemüüri app. Kasuta seda moodulit tuleks piirata ainult kogenud kasutajatele.';
