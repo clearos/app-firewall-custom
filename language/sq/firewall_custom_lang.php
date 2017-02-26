ea6c452a9692c263aebc6b517460d725bbb8f5c8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:41:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'App Custom Firewall siguron një mjet të nivelit të ulët për të konfiguruar rregullat avancuara firewall.';
+$lang['firewall_custom_app_name'] = 'Custom Firewall';
+$lang['firewall_custom_command_is_not_permitted'] = 'Komanda nuk lejohet.';
+$lang['firewall_custom_description_invalid'] = 'Përshkrimi është i pavlefshëm.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'Rregulli firewall është i pavlefshëm.';
+$lang['firewall_custom_help_dragging'] = 'Urdhri i rregullave tuaja firewall doganore është e rëndësishme - kliko dhe terhiq një rregull lart ose poshtë në tabelë për të ndryshuar rendin.';
+$lang['firewall_custom_help_iptables_constant'] = '$ Iptables është një konstante për iptables komandën që përfshin flamuj për të parandaluar fotografi mbyllje çështje. Ju lutemi përdorni këtë konstante, kur të jetë e mundur!';
+$lang['firewall_custom_restart_required'] = 'Urdhri i rregullave tuaja mund të ketë ndryshuar - një restart firewall është e nevojshme.';
+$lang['firewall_custom_summary'] = 'Lejon rregullat përshtatur firewall për të shtuar. Në mjedise të avancuar ose përshtatur shumë rrjeteve, shtimi i rregullave të avancuara firewall që nuk mund të shtohet duke përdorur aplikacionet firewall në dispozicion (në hyrje, DMZ, MultiWAN, Port Forwarding etj), për të arritur dëshiruar të sigurisë dhe pako kurs mund të realizohet duke përdorur firewall porosi app. Përdorimi i këtij moduli duhet të rezervohet vetëm për përdoruesit e përparuar.';
