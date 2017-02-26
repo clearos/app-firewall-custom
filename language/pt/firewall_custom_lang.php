69df51ebd66464d9f6bf1c3b7c9f3caacd11cfc7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:49:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pt/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'O aplicativo personalizado Firewall fornece uma ferramenta de baixo nível para configurar regras de firewall avançados.';
+$lang['firewall_custom_app_name'] = 'Firewall Personalizado';
+$lang['firewall_custom_command_is_not_permitted'] = 'Comando não é permitido.';
+$lang['firewall_custom_description_invalid'] = 'Descrição é inválido.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'regra de firewall é inválido.';
+$lang['firewall_custom_help_dragging'] = 'A ordem das regras de firewall personalizados é importante - clique e arraste uma regra para cima ou para baixo na tabela para alterar a ordem.';
+$lang['firewall_custom_help_iptables_constant'] = '$ IPTABLES é uma constante para o comando iptables, que inclui bandeiras para evitar problemas de bloqueio de arquivo. Por favor, use esta constante quando possível!';
+$lang['firewall_custom_restart_required'] = 'A ordem das regras pode ter mudado - um reinício firewall é necessária.';
+$lang['firewall_custom_summary'] = 'Permite que regras de firewall personalizadas para ser adicionado. Em ambientes avançados ou altamente personalizados de redes, a adição de regras de firewall avançados que não podem ser adicionadas usando os aplicativos de firewall disponíveis (Incoming, DMZ, MultiWAN, Port Forwarding etc.) para conseguir o encaminhamento segurança e pacote desejado pode ser realizada utilizando o firewall personalizado aplicativo. A utilização deste módulo deve ser reservada apenas para usuários avançados.';
