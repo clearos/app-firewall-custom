7b870551b380b61ee0dc421e3710ae29d8aa18b4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:42:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ca/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'L&#39;aplicació de tallafocs personalitzada proporciona una eina de baix nivell per configurar regles de tallafocs avançades.';
+$lang['firewall_custom_app_name'] = 'firewall personalitzada';
+$lang['firewall_custom_command_is_not_permitted'] = 'No es permet la comanda.';
+$lang['firewall_custom_description_invalid'] = 'Descripció no és vàlid.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'de regles de tallafocs no és vàlid.';
+$lang['firewall_custom_help_dragging'] = 'L&#39;ordre de les regles de tallafocs personalitzades és important - feu clic i arrossegueu una regla amunt o cap avall a la taula per canviar l&#39;ordre.';
+$lang['firewall_custom_help_iptables_constant'] = '$ Iptables és una constant per la comanda iptables que inclou banderes per evitar problemes de bloqueig de fitxers. Si us plau, utilitzeu aquesta constant quan sigui possible!';
+$lang['firewall_custom_restart_required'] = 'L&#39;ordre de les regles pot haver canviat - Cal reiniciar el servidor de seguretat.';
+$lang['firewall_custom_summary'] = 'Permet que les regles de tallafocs personalitzades que s&#39;afegeixen. En entorns avançats o molt personalitzats de xarxes, l&#39;addició de regles de tallafocs avançades que no es poden afegir mitjançant les aplicacions de tallafocs disponibles (entrant, DMZ, MultiWAN, el reenviament de ports, etc.) per aconseguir la seguretat i enrutament de paquets desitjada es pot aconseguir utilitzant el servidor de seguretat personalitzada aplicació. L&#39;ús d&#39;aquest mòdul s&#39;ha de reservar només per a usuaris avançats.';
