aadb8621b54624e0de4a4c8203b157da2157fab5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:46:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = '사용자 지정 방화벽 응용 프로그램은 고급 방화벽 규칙을 구성 할 수있는 낮은 수준의 도구를 제공합니다.';
+$lang['firewall_custom_app_name'] = '사용자 정의 방화벽';
+$lang['firewall_custom_command_is_not_permitted'] = '명령은 허용되지 않습니다.';
+$lang['firewall_custom_description_invalid'] = '설명이 잘못되었습니다.';
+$lang['firewall_custom_firewall_rule_invalid'] = '방화벽 규칙이 잘못되었습니다.';
+$lang['firewall_custom_help_dragging'] = '사용자 정의 방화벽 규칙의 순서는 중요합니다 - 클릭하고 순서를 변경하려면 테이블에서 위 또는 아래 규칙을 끕니다.';
+$lang['firewall_custom_help_iptables_constant'] = '$ IPTABLES 파일 잠금 문제를 방지하기 위해 플래그를 포함하는 명령의 iptables 상수이다. 가능하면이 상수를 사용하십시오!';
+$lang['firewall_custom_restart_required'] = '규칙의 순서는 변경 될 수 있습니다 - 방화벽을 다시 시작해야합니다.';
+$lang['firewall_custom_summary'] = '수 있도록 사용자 정의 방화벽 규칙을 추가 할 수 있습니다. 고급 또는 고도로 맞춤화 된 네트워킹 환경에서 사용할 방화벽 애플리케이션을 사용하여 추가 할 수없는 고급 방화벽 룰의 추가 (DMZ는 MultiWAN는, 포트 포워딩 등이 들어오는) 맞춤 방화벽을 이용하여 달성 될 수있는 바람직한 보안 및 패킷 라우팅을 달성 앱. 이 모듈을 사용하면 고급 사용자 용으로 예약해야합니다.';
