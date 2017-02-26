a19b318a73609b1d6693c0a29b1fa03008e55423
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:41:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'Custom Firewall app qabaqcıl firewall qaydaları konfiqurasiya aşağı səviyyəli aracı təmin edir.';
+$lang['firewall_custom_app_name'] = 'Custom Firewall';
+$lang['firewall_custom_command_is_not_permitted'] = 'Command icazə verilmir.';
+$lang['firewall_custom_description_invalid'] = 'Təsvir yanlışdır.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'Firewall qayda etibarsızdır.';
+$lang['firewall_custom_help_dragging'] = 'xüsusi firewall qaydaları üçün vacibdir - basın və sırasını dəyişmək üçün masa yuxarı və ya aşağı bir qayda sürükleyin.';
+$lang['firewall_custom_help_iptables_constant'] = '$ IPTABLES fayl kilitleme məsələləri qarşısını almaq üçün bayraqları daxildir iptables komanda üçün daimi deyil. Mümkün bu daimi istifadə edin!';
+$lang['firewall_custom_restart_required'] = 'qaydaları qaydası dəyişmiş ola bilər - bir firewall yenidən tələb olunur.';
+$lang['firewall_custom_summary'] = 'Imkan verir xüsusi firewall qaydaları əlavə olunacaq. inkişaf etmiş və ya yüksək özelleştirilebilir şəbəkə mühitlərdə mövcud firewall apps istifadə edərək əlavə edilə bilməz qabaqcıl firewall qaydaları əlavə (DMZ, MultiWAN, Port Forwarding s Gelen) xüsusi firewall istifadə edərək həyata bilər, istədiyiniz təhlükəsizlik və paket marşrutlaşdırma nail olmaq üçün app. Bu modul istifadə yalnız qabaqcıl istifadəçilər üçün olmalıdır.';
