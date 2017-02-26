7da1ba3d5ae47b2db66bf158b97a0b2615387dc5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:42:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bs/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'Aplikacija Custom Firewall pruža alat niskog nivoa konfigurirati napredne firewall.';
+$lang['firewall_custom_app_name'] = 'Custom Firewall';
+$lang['firewall_custom_command_is_not_permitted'] = 'Komanda nije dozvoljeno.';
+$lang['firewall_custom_description_invalid'] = 'Opis je nevažeći.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'Firewall pravilo je nevažeća.';
+$lang['firewall_custom_help_dragging'] = 'Redoslijed prilagođeni firewall je važno - kliknite i povucite pravilo gore ili dolje u tabeli promijeniti poredak.';
+$lang['firewall_custom_help_iptables_constant'] = '$ Iptables je konstanta za naredbu iptables koji uključuje zastave za sprečavanje problema zaključavanje datoteka. Molimo Vas da koristite ovaj konstanta kada je to moguće!';
+$lang['firewall_custom_restart_required'] = 'Redoslijed vaša pravila možda su se promijenili - je potreban firewall ponovno pokretanje.';
+$lang['firewall_custom_summary'] = 'Omogućava prilagođene pravila firewall da se doda. U naprednim ili visoko prilagođene umrežavanje okruženjima, pored napredne firewall koji se ne može dodati pomoću dostupnih firewall aplikacije (dolazni, DMZ, MultiWAN, Port Forwarding itd) da se postigne željena sigurnost i usmjeravanjem paketa može se postići preko korisničkog firewall aplikacija. Korištenje ovog modula treba rezervisana za samo napredne korisnike.';
