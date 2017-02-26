a5c640554a73fbf441fe603bee56bc5f11d3387b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:42:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'စိတ်ကြိုက် Firewall ကို app ကိုအဆင့်မြင့် firewall ကစည်းမျဉ်းစည်းကမ်းတွေကို configure တစ် low-level tool ကိုပေးစွမ်းသည်။';
+$lang['firewall_custom_app_name'] = 'စိတ်တိုင်းကျ Firewall ကို';
+$lang['firewall_custom_command_is_not_permitted'] = 'စစ်ဌာနချုပ်ခွင့်ပြုမထားပေ။';
+$lang['firewall_custom_description_invalid'] = 'ဖော်ပြချက်ဟာမမှန်ကန်ဘူး။';
+$lang['firewall_custom_firewall_rule_invalid'] = 'Firewall ကိုအုပ်ချုပ်မမှန်ကန်ဘူး။';
+$lang['firewall_custom_help_dragging'] = 'သင့်ရဲ့ထုံးစံ firewall ကစည်းမျဉ်းစည်းကမ်းတွေရဲ့အမိန့်ကအရေးကြီးတယ် - ထိုအမိန့်ကိုပြောင်းလဲဖွင့်သို့မဟုတ်ဆင်း table ထဲမှာစည်းကမ်း click နဲ့ဆွဲယူပါ။';
+$lang['firewall_custom_help_iptables_constant'] = '$ iptables ဖိုင်ကိုသော့ခတ်ကိစ္စများကာကွယ်တားဆီးဖို့အလံတို့ပါဝင်သည်သော iptables command ကိုတစ်ဦးစဉ်ဆက်မပြတ်ဖြစ်ပါတယ်။ လာသောအခါဖြစ်နိုင်သမျှဒီစဉ်ဆက်မပြတ်ကိုသုံးပါ';
+$lang['firewall_custom_restart_required'] = 'သင့်ရဲ့စည်းမျဉ်းစည်းကမ်းတွေရဲ့အမိန့်ကိုပြောင်းလဲကြပေမည် - Firewall တခုက restart လုပ်လိုက်ပါလိုအပ်ပါသည်။';
+$lang['firewall_custom_summary'] = 'စိတ်ကြိုက် firewall ကစည်းမျဉ်းစည်းကမ်းတွေကဆက်ပြောသည်ခံရဖို့ခွင့်ပြုပါတယ်။ ခေတ်မီသို့မဟုတ်အလွန်အမင်းစိတ်ကြိုက်ကွန်ရက်ပတ်ဝန်းကျင်မှာ, အလိုရှိသောလုံခြုံရေးနှင့် packet ကို routing ကိုအောင်မြင်ရန်မှရရှိနိုင် firewall က apps များ (စသည်တို့ကိုအဝင်, မျဇ, MultiWAN, port forwarding) ကို အသုံးပြု. ထည့်သွင်းမရနိုငျသောအဆင့်မြင့် firewall ကစည်းမျဉ်းစည်းကမ်းတွေရဲ့ထို့အပြင်ထုံးစံ firewall ကို အသုံးပြု. ပြည့်စုံစေနိုင်သည် app ကို။ ဒီ module ကိုအသုံးပြုခြင်းသာအဆင့်မြင့်အသုံးပြုသူများအတွက် reserved ရပါမည်။';
