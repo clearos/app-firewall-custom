3f104f8f1f9172fda6a470ac6ccb841b9e4e8530
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:52:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'דער מנהג Firewall אַפּ גיט אַ נידעריק-מדרגה געצייַג צו configure אַוואַנסירטע Firewall כּללים.';
+$lang['firewall_custom_app_name'] = 'מנהג Firewall';
+$lang['firewall_custom_command_is_not_permitted'] = 'באַפֿעל איז ניט דערלויבט.';
+$lang['firewall_custom_description_invalid'] = 'באַשרייַבונג איז פאַרקריפּלט.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'Firewall הערשן איז פאַרקריפּלט.';
+$lang['firewall_custom_help_dragging'] = 'דער סדר פון דיין מנהג Firewall כּללים איז וויכטיק - גיט און שלעפּן אַ הערשן אַרויף אָדער אַראָפּ אין די טיש צו טוישן די סדר.';
+$lang['firewall_custom_help_iptables_constant'] = '$ Iptables איז אַ קעסיידערדיק פֿאַר די iptables באַפֿעל אַז כולל flags צו פאַרמייַדן טעקע לאַקינג ישוז. ביטע נוצן דעם קעסיידערדיק ווען מעגלעך!';
+$lang['firewall_custom_restart_required'] = 'דער סדר פון דיין כּללים מייַ האָבן געביטן - אַ Firewall ריסטאַרט איז required.';
+$lang['firewall_custom_summary'] = 'אַללאָווס קאַסטאַמייזד Firewall כּללים צו זייַן צוגעגעבן. אין אַוואַנסירטע אָדער העכסט קאַסטאַמייזד נעטוואָרקינג ינווייראַנמאַנץ, די דערצו פון אַוואַנסירטע Firewall כּללים אַז קענען ניט זיין צוגעגעבן ניצן די פאַראַנען Firewall אַפּס (ינקאָמינג, דיעמזי, מולטיוואַן, פּאָרט Forwarding אאז"ו ו) צו דערגרייכן די געבעטן זיכערהייַט און פּאַקאַט רוטינג קענען זיין ממלא ניצן די מנהג Firewall אַפּ. נוצן פון דעם מאָדולע זאָל זייַן רעזערווירט פֿאַר אַוואַנסירטע ניצערס נאָר.';
