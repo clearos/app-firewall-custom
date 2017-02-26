57dfd183d38a0102433fdce157f2b4a01c9d613a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:45:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'Soláthraíonn an app Custom Firewall uirlis íseal-leibhéil a chumrú rialacha balla dóiteáin chun cinn.';
+$lang['firewall_custom_app_name'] = 'Balla Dóiteáin Saincheaptha';
+$lang['firewall_custom_command_is_not_permitted'] = 'Ní Ordú ceadaithe.';
+$lang['firewall_custom_description_invalid'] = 'Is Tuairisc neamhbhailí.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'Is riail balla dóiteáin neamhbhailí.';
+$lang['firewall_custom_help_dragging'] = 'Is an t-ord do rialacha balla dóiteáin saincheaptha tábhachtach - cliceáil agus tarraing riail suas nó síos sa tábla a athrú ar an ord.';
+$lang['firewall_custom_help_iptables_constant'] = 'Is $ iptables tairiseach don ordú iptables go n-áirítear bratacha a chosc saincheisteanna comhad Glasáil. Bain úsáid as an tairiseach nuair is féidir!';
+$lang['firewall_custom_restart_required'] = 'Féadfaidh an t-ord do rialacha tar éis athrú - is atosú balla dóiteáin ag teastáil.';
+$lang['firewall_custom_summary'] = 'Ceadaíonn rialacha balla dóiteáin saincheaptha a chur leis. I dtimpeallachtaí líonraithe cinn nó an-saincheaptha, an Chomh maith le rialacha balla dóiteáin chun cinn nach féidir a chur leis ag baint úsáide as na apps balla dóiteáin ar fáil (Isteach, DMZ, MultiWAN, Aghaidh Port srl) a bhaint amach slándáil agus paicéad teastáil ródú féidir a chur i gcrích ag baint úsáide as an balla dóiteáin saincheaptha app. Ba cheart úsáid a bhaint as an modúl seo a fhorchoimeád do úsáideoirí chun cinn amháin.';
