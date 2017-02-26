171ee2242d78ca56fa4cf153d8045d8927918a33
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:45:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'Custom Firewall aplikasi menyediakan alat-tingkat rendah untuk mengkonfigurasi aturan firewall canggih.';
+$lang['firewall_custom_app_name'] = 'Firewall kustom';
+$lang['firewall_custom_command_is_not_permitted'] = 'Perintah tidak diizinkan.';
+$lang['firewall_custom_description_invalid'] = 'Deskripsi tidak valid.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'aturan firewall tidak valid.';
+$lang['firewall_custom_help_dragging'] = 'Urutan aturan firewall kustom Anda adalah penting - klik dan drag aturan atas atau bawah dalam tabel untuk mengubah urutan.';
+$lang['firewall_custom_help_iptables_constant'] = '$ IPTABLES adalah konstan untuk perintah iptables yang mencakup bendera untuk mencegah penguncian file masalah. Silakan gunakan konstan bila memungkinkan!';
+$lang['firewall_custom_restart_required'] = 'Urutan aturan mungkin telah berubah - restart firewall diperlukan.';
+$lang['firewall_custom_summary'] = 'Memungkinkan aturan firewall disesuaikan untuk ditambahkan. Dalam lingkungan jaringan canggih atau sangat disesuaikan, penambahan aturan firewall canggih yang tidak dapat ditambahkan dengan menggunakan aplikasi firewall yang tersedia (masuk, DMZ, MultiWAN, Port Forwarding dll) untuk mencapai keamanan dan routing paket yang diinginkan dapat dicapai dengan menggunakan firewall kustom aplikasi. Penggunaan modul ini harus disediakan untuk pengguna tingkat lanjut.';
