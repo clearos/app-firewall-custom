93b21634a5385dcb03f9dc9cca1ad2af46ec19b5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:43:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'La Propra Firewall app provizas malalta nivelo ilo konfiguri progresinta firewall reguloj.';
+$lang['firewall_custom_app_name'] = 'Propra Firewall';
+$lang['firewall_custom_command_is_not_permitted'] = 'Komando estas malpermesita.';
+$lang['firewall_custom_description_invalid'] = 'Priskribo estas nevalida.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'Fajroŝirmilo regulo estas nevalida.';
+$lang['firewall_custom_help_dragging'] = 'La ordon de via kutimo firewall reguloj gravas - klaku kaj treni regulon supren aŭ malsupren en la tablo por ŝanĝi la ordon.';
+$lang['firewall_custom_help_iptables_constant'] = '$ Iptables estas konstanta por la iptables komando kiu inkluzivas flagojn malhelpi dosiero blokado temoj. Bonvolu uzi ĉi tiu konstanta kiam ebla!';
+$lang['firewall_custom_restart_required'] = 'La ordon de viaj reguloj eble ŝanĝiĝis - fajroŝirmilo rekomenco postulas.';
+$lang['firewall_custom_summary'] = 'Permesas adaptitajn firewall reguloj aldoniĝos. En progresinta aŭ tre personecigitaj interkonektanta medioj, la aldono de progresinta firewall reguloj kiuj ne povas esti aldonitaj uzante la disponeblaj firewall apps (Alvenantaj, DMZ, MultiWAN Port Forwarding ktp) por atingi la deziratan sekureco kaj paketon vojigo povas esti plenumita uzante la kutimo fajroŝirmilo app. Uzo de ĉi tiu modulo devas esti rezervita por uzantoj antaŭita nur.';
