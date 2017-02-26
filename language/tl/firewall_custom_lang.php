ffec9b5e8d73f2a81d194acba7627103dc1e66d2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:51:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'Ang Custom Firewall app ay nagbibigay ng isang mababang antas ng tool na ito upang i-configure ang mga advanced na mga patakaran ng firewall.';
+$lang['firewall_custom_app_name'] = 'Custom Firewall';
+$lang['firewall_custom_command_is_not_permitted'] = 'Command ay hindi pinahihintulutan.';
+$lang['firewall_custom_description_invalid'] = 'Paglalarawan ay hindi wasto.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'Firewall patakaran ay hindi wasto.';
+$lang['firewall_custom_help_dragging'] = 'Ang pagkakasunud-sunod ng iyong pasadyang mga patakaran ng firewall ay mahalaga - i-click at i-drag ng isang patakaran ng pataas o pababa sa talahanayan upang baguhin ang pagkakasunud-sunod.';
+$lang['firewall_custom_help_iptables_constant'] = '$ IPTABLES ay isang pare-pareho para sa iptables command na may kasamang mga flag upang maiwasan ang file pagsasara ng mga isyu. Mangyaring gamitin ang constant kapag posible!';
+$lang['firewall_custom_restart_required'] = 'Ang pagkakasunud-sunod ng iyong mga patakaran ay maaaring nabago - isang firewall restart ay kinakailangan.';
+$lang['firewall_custom_summary'] = 'Pinapayagan customized patakaran ng firewall upang maidagdag. Sa mga advanced na o mataas na customized na kapaligiran networking, ang pagdaragdag ng mga advanced na mga patakaran ng firewall na hindi maaaring idagdag gamit ang mga magagamit na firewall apps (Papasok, DMZ, MultiWAN, Port Pagpapasa atbp) upang makamit ang nais na seguridad at packet routing ay maaaring maganap gamit ang custom firewall app. Paggamit ng ang module na ito ay dapat na nakalaan para sa mga advanced na user lamang.';
