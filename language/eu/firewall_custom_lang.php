438f8238c13addb54543789fb978e5f7c591a72b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:42:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'Firewall pertsonalizatua Aplikazioa behe-mailako tresna bat firewall arau aurreratuak konfiguratzeko aukera ematen du.';
+$lang['firewall_custom_app_name'] = 'Firewall pertsonalizatua';
+$lang['firewall_custom_command_is_not_permitted'] = 'Komandoa ez da onartzen.';
+$lang['firewall_custom_description_invalid'] = 'Description baliogabea da.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'Firewall arau baliogabea da.';
+$lang['firewall_custom_help_dragging'] = 'Zure Ohiko suebakiaren arauen ordena garrantzitsua da - egin klik eta arrastatu arau bat gora edo behera mahai ordena aldatzeko.';
+$lang['firewall_custom_help_iptables_constant'] = '$ Iptables iptables komandoa biltzen dituen banderak fitxategi blokeoa gaiak saihesteko konstante bat da. Mesedez, erabili konstante hau posible denean!';
+$lang['firewall_custom_restart_required'] = 'Zure arauen ordena aldatu egin daiteke - firewall berrabiaraztea beharrezkoa da.';
+$lang['firewall_custom_summary'] = 'Aukera ematen pertsonalizatu firewall arauak gehitu behar zaizkio. networking ingurune aurreratu edo oso pertsonalizatuak ere, firewall arau aurreratuak gain ezin dela gehitu du eskuragarri firewall aplikazioak erabiliz (Incoming, DMZ, MultiWAN, Port Forwarding abar) nahi duzun segurtasun eta pakete bideraketa lortzeko gauzatu ahal izango dira firewall pertsonalizatuak erabiliz aplikazioa. modulu honen erabilera izango erabiltzaile aurreratuak behar erreserbatuta.';
