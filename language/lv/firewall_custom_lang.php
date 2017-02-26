7658009266856cda1cd36c90f593e4fb149de8a4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:47:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'Custom Firewall app nodrošina zema līmeņa instruments, lai konfigurētu uzlabotas ugunsmūra noteikumus.';
+$lang['firewall_custom_app_name'] = 'Custom Firewall';
+$lang['firewall_custom_command_is_not_permitted'] = 'Komanda nav atļauta.';
+$lang['firewall_custom_description_invalid'] = 'Apraksts ir nederīgs.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'Firewall noteikums ir nederīgs.';
+$lang['firewall_custom_help_dragging'] = 'No Jūsu pasūtījuma ugunsmūra noteikumu rīkojums ir svarīgi - noklikšķiniet un velciet normu augšu vai uz leju tabulā, lai mainītu secību.';
+$lang['firewall_custom_help_iptables_constant'] = '$ Iptables ir konstante iptables komandu, kas ietver karodziņus, lai novērstu failu bloķēšanas problēmas. Lūdzu, izmantojiet šo konstanti, ja iespējams!';
+$lang['firewall_custom_restart_required'] = 'No jūsu noteikumiem kārtība var būt mainīta - ir nepieciešams ugunsmūris restart.';
+$lang['firewall_custom_summary'] = 'Ļauj pielāgotus ugunsmūra noteikumi, kas jāpievieno. Uzlabotas vai augsti individualizētu tīklu vidē, tad uzlabotas ugunsmūra noteikumu papildinājums, kas nevar pievienot, izmantojot pieejamos ugunsmūra apps (Incoming, DMZ, MultiWAN, Port Forwarding uc), lai sasniegtu vēlamo drošības un pakešu maršrutēšanu var veikt, izmantojot pielāgoto ugunsmūri app. Izmantot šo moduli būtu jārezervē tikai pieredzējušiem lietotājiem.';
