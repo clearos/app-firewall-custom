f4aee187c5a8a3abf242c5367c797c3f65a1a8a4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:48:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'Il-app Custom Firewall jipprovdi għodda ta &#39;livell baxx biex jiġi kkonfigurat regoli firewall avvanzati.';
+$lang['firewall_custom_app_name'] = 'Firewall custom';
+$lang['firewall_custom_command_is_not_permitted'] = 'Kmand mhux permess.';
+$lang['firewall_custom_description_invalid'] = 'Deskrizzjoni hija invalida.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'regola Firewall huwa invalidu.';
+$lang['firewall_custom_help_dragging'] = 'L-ordni ta &#39;regoli tiegħek firewall custom huwa importanti - ikklikkja u drag regola jew&#39; l isfel fit-tabella li jibdlu l-ordni.';
+$lang['firewall_custom_help_iptables_constant'] = '$ Iptables huwa kostanti għall-kmand iptables li tinkludi bnadar li jipprevjenu kwistjonijiet fajl qfil. Jekk jogħġbok uża din il-kostanti meta jkun possibbli!';
+$lang['firewall_custom_restart_required'] = 'L-ordni ta &#39;regoli tiegħek jistgħu jkunu nbidlu - jistartja firewall hija meħtieġa.';
+$lang['firewall_custom_summary'] = 'Jippermetti regoli firewall personalizzati biex jiġu miżjuda. F&#39;ambjenti avvanzat jew ħafna customized netwerking, iż-żieda ta &#39;regoli firewall avvanzati li ma jistgħux jiġu miżjuda bl-użu apps firewall disponibbli (incoming, DMZ, MultiWAN, Port Tressiq eċċ) biex tinkiseb is-sigurtà u l-pakkett mixtieq rotta jista&#39; jitwettaq bl-użu firewall dwana app. Użu ta &#39;dan il-modulu għandha tiġi riservata għall-utenti avvanzati biss.';
