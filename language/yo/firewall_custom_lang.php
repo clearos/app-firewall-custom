b3f9adb18c09ec282289facc9f6538ac9e426ab6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:53:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'Awọn Aṣa Firewall app pese a kekere-ipele ọpa lati tunto ti ni ilọsiwaju ogiriina ofin.';
+$lang['firewall_custom_app_name'] = 'aṣa Ogiriina';
+$lang['firewall_custom_command_is_not_permitted'] = 'Òfin ti wa ni ko yọọda.';
+$lang['firewall_custom_description_invalid'] = 'Apejuwe ni invalid.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'Ogiriina ofin ni invalid.';
+$lang['firewall_custom_help_dragging'] = 'Aṣẹ ti rẹ aṣa ogiriina ofin jẹ pataki - tẹ ki o si fa a ofin soke tabi isalẹ ninu tabili lati yi awọn ibere.';
+$lang['firewall_custom_help_iptables_constant'] = '$ IPTABLES ni a ibakan fun awọn iptables aṣẹ ti o ba pẹlu awọn asia lati se faili tilekun oran. Jọwọ lo yi ibakan nigbati o ti ṣee!';
+$lang['firewall_custom_restart_required'] = 'Awọn ibere ti rẹ ofin le ti yi pada - a ogiriina bẹrẹ wa ni ti beere.';
+$lang['firewall_custom_summary'] = 'Gba awọn ti adani ogiriina ofin to wa ni fi kun. Ni ti ni ilọsiwaju tabi gíga ti adani Nẹtiwọki agbegbe, awọn afikun ti to ti ni ilọsiwaju ogiriina awọn ofin ti o le wa ko le fi kun lilo awọn wa ogiriina apps (ti nwọle, DMZ, MultiWAN, Port Ndari awọn ati be be lo) lati se aseyori awọn ti o fẹ aabo ati soso afisona le se lilo awọn aṣa ogiriina app. Lilo ti yi module yẹ ki o wa ni ipamọ fun to ti ni ilọsiwaju awọn olumulo nikan.';
