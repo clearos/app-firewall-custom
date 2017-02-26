cd4de315f4b95acf0ee20c7f54d97a7ecb52b164
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 08:43:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/da/firewall_custom_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang['firewall_custom_app_description'] = 'Custom Firewall app giver et lavt niveau værktøj til at konfigurere avancerede firewall-regler.';
+$lang['firewall_custom_app_name'] = 'Tilpasset Firewall';
+$lang['firewall_custom_command_is_not_permitted'] = 'Kommando er ikke tilladt.';
+$lang['firewall_custom_description_invalid'] = 'Beskrivelse er ugyldig.';
+$lang['firewall_custom_firewall_rule_invalid'] = 'Firewall regel er ugyldig.';
+$lang['firewall_custom_help_dragging'] = 'Rækkefølgen af ​​dine brugerdefinerede firewall regler er vigtig - klik og træk en regel op eller ned i tabellen for at ændre rækkefølgen.';
+$lang['firewall_custom_help_iptables_constant'] = '$ IPTABLES er en konstant for iptables kommando, der indeholder flag for at forhindre fil låsning spørgsmål. Brug venligst denne konstant når det er muligt!';
+$lang['firewall_custom_restart_required'] = 'Rækkefølgen af ​​dine regler kan have ændret - en firewall genstart er påkrævet.';
+$lang['firewall_custom_summary'] = 'Tillader tilpassede firewall regler, der skal tilføjes. I avancerede eller meget tilpassede netværksmiljøer, tilføjelse af avancerede firewall-regler, der kan ikke tilføjes ved hjælp af de tilgængelige firewall apps (Indgående, DMZ, MultiWAN, Port Forwarding etc.) for at opnå den ønskede sikkerhed og pakke routing kan opnås ved hjælp af brugerdefinerede firewall app. Brug af dette modul bør være forbeholdt kun avancerede brugere.';
